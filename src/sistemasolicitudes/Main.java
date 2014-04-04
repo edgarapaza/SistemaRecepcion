9763a8acee134601ce857b116ce720165a670e11
Edgar <edgarapazac@hotmail.com>
2014-04-04 10:01:05 -0500

    actualizando texto

--- src/sistemasolicitudes/Main.java
+++ src/sistemasolicitudes/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         frmLogin Login=new frmLogin();
         Login.setVisible(true);
+        // Devolver un arespuesta
     }
 
 }
